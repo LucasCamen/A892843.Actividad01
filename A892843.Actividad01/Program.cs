e516e99b5e01e06852677b255d6b300c12e55837
Lucas Camen <lucas.camen98@gmail.com>
2021-03-18 19:24:33 -0300

    .

---
+++ A892843.Actividad01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A892843.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
